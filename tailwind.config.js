cb653b709a5af38c511bd6f74a52c2f36d3918f8
precompile <xenxenny@gmail.com>
2023-01-30 21:01:04 +0200

    Color theme changes v0.2.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./build/*.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      black: '#0a0a0a',
+      white: '#f3f3f3',
+    }
   },
   plugins: [
     require('@tailwindcss/typography')
